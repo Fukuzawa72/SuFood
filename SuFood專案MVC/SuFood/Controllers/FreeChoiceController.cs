c30db6cdcb6d0d7076ce013e8d0fb935a6659faf
MSI\chong <aa>
2023-05-20 23:31:31 +0800

    The first MVC Project of SuFood.

---
+++ SuFood專案MVC/SuFood/Controllers/FreeChoiceController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using SuFood.Models;
+using SuFood.ViewModel;
+
+namespace SuFood.Controllers
+{
+    public class FreeChoiceController : Controller
+    {
+        private readonly SuFoodDBContext _context;
+
+        public FreeChoiceController(SuFoodDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult FreeChoice()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<VmProductForFront>> GetProducts()
+        {
+            return _context.Products.Select(x => new VmProductForFront
+            {
+                ProductId= x.ProductId,
+                ProductName= x.ProductName,
+                Price= x.Price,
+                Img = x.Img,
+                Quantity = 0
+            });
+        }
+    }
+}

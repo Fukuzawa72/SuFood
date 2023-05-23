c39c2bf4b3adaef1a57d553fa59cde5717a0ba49
MSI\chong <aa>
2023-05-24 01:12:24 +0800

    append UI of freeChoicePlan, already. can retrieve data from the backend. The remaining methods for create, post, and delete are yet to be implemented.

---
+++ SuFood專案MVC/SuFood/Areas/BackStage/Controllers/FreeChoicePlansManagementController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuFood.Models;
+using SuFood.ViewModel;
+
+namespace SuFood.Areas.BackStage.Controllers
+{
+    [Area("BackStage")]
+    public class FreeChoicePlansManagementController : Controller
+    {
+        private readonly SuFoodDBContext _context;
+
+        public FreeChoicePlansManagementController(SuFoodDBContext context)
+        {
+            _context = context;
+        }
+
+        //GET: /BackStage/FreeChoicePlansManagement/GetPlansAndProducts 取得方案與方案內的商品 !!有三張表的寫法
+        public Object GetPlansAndProducts()
+        {
+            return _context.ProductsOfPlans.GroupBy(p => p.PlanId).Select(group => new
+            {
+                plans = group.Select(p => new VmFreeChoicePlans
+                {
+                    PlanId = p.Plan.PlanId,
+                    PlanName = p.Plan.PlanName,
+                    PlanDescription = p.Plan.PlanDescription,
+                    PlanPrice = p.Plan.PlanPrice,
+                    PlanTotalCount = p.Plan.PlanTotalCount,
+                    PlanCanChoiceCount = p.Plan.PlanCanChoiceCount,
+                    PlanStatus = p.Plan.PlanStatus,
+                }).SingleOrDefault(),
+                Products = group.Select(p => new VmProductForFront
+                {
+                    ProductId = p.Product.ProductId,
+                    ProductName = p.Product.ProductName,
+                    Price = p.Product.Price,
+                    Img = p.Product.Img,
+                    Quantity = 0
+                })
+            });
+        }
+
+        //GET: /BackStage/FreeChoicePlansManagement/GetAllProducts 取得所有商品
+        [HttpGet]
+        public async Task<IEnumerable<object>> GetAllProducts()
+        {
+            return _context.Products.Select(pro => new
+            {
+                ProductId = pro.ProductId,
+                ProductName = pro.ProductName,
+                Price = pro.Price,
+            });
+        }
+
+        //DELETE: /FreeChoicePlansManagement/DeleteProductsOfPlans 刪除方案中的產品(單筆)
+        [HttpDelete]
+        public async Task<string> DeleteProductsOfPlans(int PlanId, int ProductId)
+        {
+            if (_context.ProductsOfPlans == null)
+            {
+                return "資料不存在";
+            }
+
+            var product = await _context.ProductsOfPlans.FindAsync(PlanId, ProductId);
+            if (product != null)
+            {
+                _context.ProductsOfPlans.Remove(product);
+            }
+            await _context.SaveChangesAsync();
+            return "刪除成功";
+        }
+
+        //POST: /FreeChoicePlansManagement/CreateProductsOfPlan 新增多筆方案中的產品
+        [HttpPost]
+        public async Task<string> CreateProductsOfPlan(List<ProductsOfPlans> products)
+        {
+            foreach (var product in products)
+            {
+                if (!_context.FreeChoicePlans.Any(p => p.PlanId == product.PlanId))
+                {
+                    return "不存在該方案";
+                }
+
+                ProductsOfPlans insert = new ProductsOfPlans
+                {
+                    PlanId = product.PlanId,
+                    ProductId = product.ProductId
+                };
+
+                _context.ProductsOfPlans.Add(insert);
+            }
+
+            await _context.SaveChangesAsync();
+            return "新增成功";
+        }
+    }
+}
---
+++ SuFood專案MVC/SuFood/wwwroot/js/freeChoicePlansManagement.js
@@ -0,0 +1,177 @@
+﻿const vm = new Vue({
+    el: ".Recent-order",
+    data: {
+        products: [],
+        plans: [],
+        selectedProducts: [],
+        uplodaImgPreview: {
+            preview: null,
+            image: null,
+        },
+        EditProductsList: {
+            planId: "",
+            planName: "",
+            planDescription: "",
+            planTotalCount: "",
+            planCanChoiceCount: "",
+            planPrice: "",
+        },
+        EditOrDeleteorCreate: "Edit",
+        modalContentStyle: {
+            w200: true,
+            w800: false
+        },
+        toastHintStlye: {
+            fadeInUp: false
+        },
+        modalContainerStyle: {
+            showModal: false,
+        },
+
+        //新方法
+        SelectedItemStyle: {
+            selected: false
+        }
+    },
+    computed: {
+        ModalTitle() {
+            switch (this.EditOrDeleteorCreate) {
+                case "Edit":
+                    return "修改"
+                    break;
+                case "Create":
+                    return "新增"
+                    break;
+                case "Delete":
+                    return "刪除"
+                    break;
+            }
+        },
+    },
+    methods: {
+        previewImage: function (event) {
+            console.log("change")
+            var input = event.target;
+            if (input.files) {
+                var reader = new FileReader();
+                reader.onload = (e) => {
+                    this.uplodaImgPreview.preview = e.target.result;
+                }
+                this.uplodaImgPreview.image = input.files[0];
+                reader.readAsDataURL(input.files[0]);
+            }
+        },
+        EditProducts(item) {
+            this.GetProducts(item.planId)
+
+            //加isSelected樣式
+            this.autoSelectAllProductsFitCondition()
+
+            this.GetAllProduct()
+            this.EditProductsList.planId = item.planId
+            this.EditProductsList.planName = item.planName
+            this.EditProductsList.planDescription = item.planDescription
+            this.EditProductsList.planTotalCount = item.planTotalCount
+            this.EditProductsList.planCanChoiceCount = item.planCanChoiceCount
+            this.EditProductsList.planPrice = item.planPrice
+            this.uplodaImgPreview.preview = item.img
+            this.EditOrDeleteorCreate = "Edit"
+            this.modalContentStyle.w200 = false
+            this.modalContentStyle.w800 = true
+            this.openModal()
+        },
+        DeleteProducts() {
+            this.EditOrDeleteorCreate = "Delete"
+            this.modalContentStyle.w200 = true
+            this.modalContentStyle.w800 = false
+            this.openModal()
+        },
+        CreateProducts() {
+            this.selectedProducts = []
+            this.GetAllProduct()
+            this.EditOrDeleteorCreate = "Create"
+            this.modalContentStyle.w200 = false
+            this.modalContentStyle.w800 = true
+            this.uplodaImgPreview.preview = null
+            this.openModal()
+        },
+        toastHint() {
+            this.toastHintStlye.fadeInUp = true
+            setTimeout(() => {
+                this.toastHintStlye.fadeInUp = false
+            }, 2100)
+
+            this.EditProductsList.planId = ""
+            this.EditProductsList.planName = ""
+            this.EditProductsList.planDescription = ""
+            this.EditProductsList.planTotalCount = ""
+            this.EditProductsList.planCanChoiceCount = ""
+            this.EditProductsList.planPrice = ""
+        },
+        openModal() {
+            this.modalContainerStyle.showModal = true;
+        },
+        closeModal() {
+            this.modalContainerStyle.showModal = false;
+        },
+        closeModalWithHint() {
+            this.modalContainerStyle.showModal = false;
+            this.toastHint();
+        },
+        //將所有商品放入選擇商品
+        toggleProductSelection(product) {
+            const index = this.selectedProducts.indexOf(product);
+            if (index !== -1) {
+                this.selectedProducts.splice(index, 1);
+            } else {
+                this.selectedProducts.push(product);
+            }
+        },
+        //方案選擇商品顏色提示
+        isSelected(product) {
+            return this.selectedProducts.includes(product);
+        },
+        //確認資料庫產品資料自動加上顏色提示
+        autoSelectAllProductsFitCondition() {
+            let spi = this.selectedProducts.map(p => p.productId)
+            let pi = this.products.map(p => p.productId)
+
+            pi.forEach(p => {
+                if (spi.includes(p)) {
+                    this.toggleProductSelection(p)
+                    document.getElementById(`li${p}`).classList.toggle("SelectedItemStyle") 
+                }
+            })
+        },
+        //查詢方案
+        GetPlans() {
+            axios.get("/BackStage/FreeChoicePlansManagement/GetPlansAndProducts").then(response => {
+                response.data.forEach(response => {
+                    console.log(response)
+                    this.plans.push(response.plans)
+                })
+            })
+        },
+        //查詢方案內的產品
+        GetProducts(planId) {
+            let _this = this;
+            axios.get("/BackStage/FreeChoicePlansManagement/GetPlansAndProducts").then(response => {
+                response.data.forEach(response => {
+                    if (response.plans.planId == planId) {
+                        _this.selectedProducts = response.products
+                    }
+                })
+            })
+        },
+        //查詢所有產品
+        GetAllProduct() {
+            let _this = this;
+            axios.get("/BackStage/FreeChoicePlansManagement/GetAllProducts").then(response => {
+                this.products = response.data
+            })
+        },
+    },
+    mounted() {
+        this.GetPlans();
+    }
+})
